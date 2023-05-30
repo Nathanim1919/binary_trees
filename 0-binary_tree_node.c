59ae5e8814b284b377d5dd03b808f32b08a1c849
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-30 03:05:04 -0700

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,11 +11,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 	
 	if (!newNode)
-		return NULL;
+		return (NULL);
 
 	newNode->n = value;
 	newNode->parent = parent;
-	newNode->left = newNode->right = NULL;
+	newNode->left = NULL;
+	newNode->right = NULL;
 	
 	return (newNode);
 }
