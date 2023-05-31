817f4fcc6607d28c84865327988532f42ca5069f
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-31 00:30:11 -0700

    is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,11 +7,11 @@
  * if node is NULL, return 0
  */
 
-binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (node == NULL)
+		return (0)
 	if (node->left == NULL && node->right == NULL)
 		return (1);
-	if (node == NULL)
-		return (0);
 	return (0);
 }
