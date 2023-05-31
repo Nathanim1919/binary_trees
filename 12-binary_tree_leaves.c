3d43e44ca6b90baa750b8b63e0e0258827251fac
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-31 10:47:45 -0700

    Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,7 +14,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
