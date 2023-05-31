eebda015eb417aa4b986fa2bb9653a21adac5412
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-31 01:15:38 -0700

    pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,7 +3,7 @@
  * binary_tree_preorder - a function that goes through
  * a binary tree using pre-order traversal
  * @tree:  is a pointer to the root node of the tree to traverse
- * @fun:  is a pointer to a function to call for each node
+ * @func:  is a pointer to a function to call for each node
  * if tree or func is NULL, do nothing
  */
 
