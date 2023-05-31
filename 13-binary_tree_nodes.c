4bdae63847b375cef7300c87fe2e9314b836ab7d
Nathanim1919 <nathanim2tadele@gmail.com>
2023-05-31 11:27:22 -0700

    count nodes with maximum of one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,3 +15,4 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
