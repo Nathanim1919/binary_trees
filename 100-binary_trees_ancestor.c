632aaf9829b678887e83b372f6a9f755aabe30a1
amanabay <amanuelabay22@gmail.com>
2023-06-01 06:52:16 +0000

    Resort to recursive approach

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,44 +11,20 @@ size_t binary_tree_depth(const binary_tree_t *tree);
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
+	binary_tree_t *parent1 = first->parent;
+	binary_tree_t *parent2 = second->parent;
+
 	if (first == NULL || second == NULL)
 		return NULL;
-
-	int depth_first = binary_tree_depth(first);
-	int depth_second = binary_tree_depth(second);
 	
-	binary_tree_t *p = (binary_tree_t *)first;
-	binary_tree_t *q = (binary_tree_t *)second;
+	if (first == second)
+		return (binary_tree_t *)first;
+	
+	if (first == parent2 || parent1 == NULL || (!parent1->parent && parent2))
+		return binary_trees_ancestor(first, parent2);
 
- 	while (depth_first > depth_second)
-	{
-		p = p->parent;
-		depth_first--;
-	}
-	while (depth_second > depth_first)
-	{
-		q = p->parent;
-		depth_second--;
-	}
-
-	while (p != q)
-	{
-		if (p == NULL || q == NULL)
-			return (NULL);
-		p = p->parent;
-		q = q->parent;
-	}
-
-	return ((binary_tree_t *)p);
-}
-
-/**
- * depth - depth of a node in a binary tree
- * @tree: node to measure length
- *
- * Return: depth of node, else 0
- */
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	return ((tree->parent != NULL) ? 1 + binary_tree_depth(tree->parent) : 0);
+	if (parent1 == second || parent2 == NULL || (!parent2->parent && parent1))
+		return binary_trees_ancestor(parent1, second);
+    
+	return (binary_trees_ancestor(parent1, parent2));
 }
