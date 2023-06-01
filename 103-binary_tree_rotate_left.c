02c680d3b3d0d0bc4c15d9139798ced046c840e3
amanabay <amanuelabay22@gmail.com>
2023-06-01 21:52:24 +0000

    Betty fix

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -14,6 +14,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 		return (tree);
 
 	binary_tree_t *new_root = tree->right;
+
 	tree->right = new_root->left;
 
 	if (new_root->left != NULL)
