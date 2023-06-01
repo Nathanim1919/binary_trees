711aca805a8beffd7f9f716d9bbef7533513fabb
amanabay <amanuelabay22@gmail.com>
2023-06-01 01:52:26 +0000

    UPdate to include ancestor function

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,7 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -41,6 +42,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
 #endif
