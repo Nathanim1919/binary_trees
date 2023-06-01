ad820ba36b652a1bb4bff65ded2300778dda9828
amanabay <amanuelabay22@gmail.com>
2023-06-01 21:49:10 +0000

    Improved rotate left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -14,8 +14,14 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 		return (tree);
 
 	binary_tree_t *new_root = tree->right;
-
 	tree->right = new_root->left;
+
+	if (new_root->left != NULL)
+		new_root->left->parent = tree;
+
 	new_root->left = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
+
 	return (new_root);
 }
