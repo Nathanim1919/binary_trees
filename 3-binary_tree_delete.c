7536550e5863e84cb0efc4608c688e7c09ef7569
amanabay <amanuelabay22@gmail.com>
2023-06-02 01:21:48 +0000

    UPdate description

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete -  function that deletes an entire binary tree
+ * binary_tree_delete - function that deletes an entire binary tree
  * @tree: is a pointer to the node of the tree to delete
  * Return: nothing
  */
