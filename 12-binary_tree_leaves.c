e9e168e903698430bbafb7f18bbe33bd44656ce1
amanabay <amanuelabay22@gmail.com>
2023-06-02 01:31:42 +0000

    Update description

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,9 +3,10 @@
  * binary_tree_leaves - counts the leaves in abinary tree
  * @tree: is apointer to the root node of the tree to count
  * the number of leaves
+ *
  * Return: the number of leaves
- * if tree is NULL, the function must return 0
- * A NULL pointer is not leaf
+ *	if tree is NULL, the function must return 0
+ *	A NULL pointer is not leaf
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
