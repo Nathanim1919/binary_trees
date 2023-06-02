61f7e647c6de618ab8fe0db30fee9dde521acafb
amanabay <amanuelabay22@gmail.com>
2023-06-02 01:34:17 +0000

    Remove redundant function - 2

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,25 +1,5 @@
 #include "binary_trees.h"
 
-
-/**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height of
- *
- * Return: the height of the tree. If tree is NULL, return 0
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left, right;
-
-	if (tree == NULL)
-		return (0);
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-	if (left >= right)
-		return (1 + left);
-	return (1 + right);
-}
-
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor of
