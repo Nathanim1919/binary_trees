c5f6cd856e102ee04d8e0de2cac6976761f18a3d
amanabay <amanuelabay22@gmail.com>
2023-06-02 01:18:22 +0000

    Remove extra function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,26 +2,6 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_node_1 - creates a binary tree node
- * @parent: parnt of the node to create
- * @value: value to store in new node
- *
- * Return: pointer to the new node
- */
-binary_tree_t *binary_tree_node_1(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node;
-
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = new_node->right = NULL;
-	return (new_node);
-}
-
-/**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
@@ -37,7 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	left_child = binary_tree_node_1(parent, value);
+	left_child = binary_tree_node(parent, value);
 	if (left_child == NULL)
 		return (NULL);
 	left_child->left = parent->left;
